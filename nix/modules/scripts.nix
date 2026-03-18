1d46e09807d31d60d72ad2fa5feeff74bc5344f6
Jon Gao <jon.gao@databricks.com>
2026-03-18 06:46:15 +0000

    Updating files

--- nix/modules/scripts.nix
+++ nix/modules/scripts.nix
@@ -16,4 +16,12 @@
   home.file."bin/tmux-validate-save" = {
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/bin/tmux-validate-save";
   };
+
+  home.file."bin/ruflo-daemon" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/bin/ruflo-daemon";
+  };
+
+  home.file."bin/ruflo-http-server.mjs" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/bin/ruflo-http-server.mjs";
+  };
 }
