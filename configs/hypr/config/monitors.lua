f749c74f35ac60067aa1c474c833485b5d9292e4
Jon <jonathan-gao@hotmail.com>
2026-06-27 00:50:08 -0700

    chore: configure DP-3 monitor to run at 144Hz

--- configs/hypr/config/monitors.lua
+++ configs/hypr/config/monitors.lua
@@ -1,8 +1,8 @@
 -- Montior wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
 
 hl.monitor({
-    output    = "",
-    mode      = "preferred",
-    position  = "auto",
-    scale     = "auto",
+    output    = "DP-3",
+    mode      = "3840x2160@144",
+    position  = "0x0",
+    scale     = "1",
 })
